da90b90ca4bc65bd463ca288993b553b9b5ac8c8
Rodrigo Grisolía <grisoliarodrigo@gmail.com>
2017-07-03 15:59:47 -0300

    Fix constructor ListadoPropiedadesCtrl

--- app/modules/propiedades/controllers/listadoPropiedadesCtrl.js
+++ app/modules/propiedades/controllers/listadoPropiedadesCtrl.js
@@ -1,16 +1,17 @@
 class ListadoPropiedadesCtrl {
   
-  constructor(PropiedadesHome) {
+  constructor(propiedades, PropiedadesHome) {
+    this.propiedades = propiedades
     this.PropiedadesHome = PropiedadesHome
   }
 
   delete (propiedadId) {
   	this.PropiedadesHome.delete(propiedadId)
-  }
+	}
   
 }
 
 angular.module("booking-app")
-.controller("ListadoPropiedadesCtrl", ListadoPropiedadesCtrl);
+.controller("ListadoPropiedadesCtrl", ListadoPropiedadesCtrl)
 
-ListadoPropiedadesCtrl.$inject = [ "propiedades", "PropiedadesHome" ];
+ListadoPropiedadesCtrl.$inject = [ "propiedades", "PropiedadesHome" ]
