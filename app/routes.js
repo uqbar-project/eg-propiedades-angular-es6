646aba440425be67677e16929acadd258995297e
Rodrigo Grisolía <grisoliarodrigo@gmail.com>
2017-07-05 12:08:18 -0300

    Migración ES6

--- app/routes.js
+++ app/routes.js
@@ -1,19 +1,18 @@
-'use strict';
-
 angular.module('booking-app', ['ui.router'])
-.config(function($urlRouterProvider, $locationProvider) {
-  $urlRouterProvider.otherwise('/');
-  return $locationProvider.html5Mode(true);
+
+.config(($urlRouterProvider, $locationProvider)  =>{
+  $urlRouterProvider.otherwise('/')
+  return $locationProvider.html5Mode(true)
 })
-.run(function($state){});  // https://github.com/angular-ui/ui-router/issues/679
+.run(($state)=> {})  // https://github.com/angular-ui/ui-router/issues/679
 
 angular.module('booking-app')
-.config(function($stateProvider) {
+.config(($stateProvider) => {
   return $stateProvider
   .state('dashboard', {
     url: "/",
     templateUrl: "app/modules/dashboard/dashboard.html",
     controller: "DashboardCtrl",
     controllerAs:"dashboardCtrl"
-  });
-});
+  })
+})
